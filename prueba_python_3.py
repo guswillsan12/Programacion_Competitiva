b1717f211cd3b5bc114786b3b08f1ae3d9010861
Gustavo Sanchez <guswillsan@gmail.com>
2023-02-14 22:50:08 -0500

    Nuevo ejercicio de python

---
+++ prueba_python_3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#Decoración: Nombre del Algoritmo
+print("-------------------------------------------------------")
+print("Ejercicio3: PUNTAJE FINAL.")
+print("-------------------------------------------------------")
+#Entradas
+print("Ingrese número de respuestas correctas: ")
+RC = int( input())
+print("Ingrese número de respuestas incorrectas: ")
+RI = int( input())
+print("Ingrese número de respuestas en blanco: ")
+RB = int( input())
+#Proceso
+PCR = RC*3
+PRI = RI*-1
+PRB = RB*0
+PF = PCR + PRI + PRB
+#Salida
+print("PCR:",PCR," PRI:",PRI, " PRB:",PRB, " Datos totales:",PF)
